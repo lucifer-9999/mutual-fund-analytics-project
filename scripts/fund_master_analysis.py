7cb82a35d8abd1383c01fd0a393f830734fd77c4
Ansh Jain <anshjain1j@gmail.com>
2026-06-04 23:24:38 +0530

    Day 1 complete

---
+++ scripts/fund_master_analysis.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv("data/raw/01_fund_master.csv")
+
+print("\nUnique Fund Houses:")
+print(df["fund_house"].nunique())
+print(df["fund_house"].unique())
+
+print("\nUnique Categories:")
+print(df["category"].unique())
+
+print("\nUnique Sub Categories:")
+print(df["sub_category"].unique())
+
+print("\nUnique Risk Grades:")
+print(df["risk_category"].unique())
